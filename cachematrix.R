2c69aa7fdffc92eab30cfd5ea1a6b4da116a7600
jc <jasoncheang@rmbp-uberadmins-MacBook-Pro.local>
2015-04-27 04:23:15 +1000

    Added solution to R file.
    Added solution to R file.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,9 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+            x <<- y
+      }
+      list(set = set, get = get,
 }
 
 
@@ -12,4 +14,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getInverse()
+		if(!is.null(m)) {
+		    message("getting cached data")
+		    return(m)
+		}
+		data <- x$get()
+		m <- solve(data, ...)
+		x$setInverse(m)
+		m
 }
